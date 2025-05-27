8583555684c12163e5c36a96eb911747ed7bee45
yeshapan <yesha.pandya@outlook.com>
2025-05-27 18:27:57 +0530

    switched to logistic regression model - with C=5.0 giving 82.86% accuracy

--- categorizer/model.py
+++ categorizer/model.py
@@ -4,12 +4,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer #to convert text to 
 from sklearn.naive_bayes import MultinomialNB #niave bayes classifier for text data
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 def train_model(df):
     X = df["text"]
     y = df["category"]
 
-    vectorizer = TfidfVectorizer(max_features=5000) #convert text to nums to feed ML model (only 5000 most imp words used)
+    vectorizer = TfidfVectorizer(max_features=5000) #convert text to nums to feed ML model (only 5000 most imp words used to prevent overfitting)
     #Tf: term frequency (frequency of any word)
     #idf: Inverse document frequency (for unique/imp any word is across all docs)
     X_vec = vectorizer.fit_transform(X)
@@ -17,8 +18,12 @@ def train_model(df):
     #train-test split
     X_train, X_test, y_train, y_test = train_test_split(X_vec, y, test_size=0.2, random_state=42)
     
-    #NB classifier
+    '''#NB classifier
     model = MultinomialNB()
+    model.fit(X_train, y_train)'''
+
+    #Logistic regressor
+    model=LogisticRegression(max_iter=1000, solver= "liblinear", C=5.0)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
