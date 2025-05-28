beac183bcbd4def1409fc8ef8c1007a15cc1b1d6
yeshapan <yesha.pandya@outlook.com>
2025-05-28 16:58:48 +0530

    Random Forest Classifier also only giving  67.39% accuracy

--- categorizer/model.py
+++ categorizer/model.py
@@ -1,17 +1,18 @@
-#using Naive Bayes for now
+#trying with Random Forest Classifier
 
 from sklearn.feature_extraction.text import TfidfVectorizer #to convert text to tf-idf feature matrix
-from sklearn.naive_bayes import MultinomialNB #niave bayes classifier for text data
+#from sklearn.naive_bayes import MultinomialNB #niave bayes classifier for text data
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 def train_model(df):
     X = df["text"]
     X=X.dropna()
     y = df["category"]
 
-    vectorizer = TfidfVectorizer(max_features=5000) #convert text to nums to feed ML model (only 5000 most imp words used to prevent overfitting)
+    vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1,2)) #convert text to nums to feed ML model (only 5000 most imp words used to prevent overfitting)
     #Tf: term frequency (frequency of any word)
     #idf: Inverse document frequency (for unique/imp any word is across all docs)
     X_vec = vectorizer.fit_transform(X)
@@ -23,10 +24,12 @@ def train_model(df):
     model = MultinomialNB()
     model.fit(X_train, y_train)'''
 
-    #Logistic regressor
+    ''' #Logistic regressor
     model=LogisticRegression(max_iter=1000, solver= "liblinear", C=5.0, penalty= "l1", class_weight="balanced")
     #C is to tune regularization
-    '''C=1.0 gives 65.71% accuracy ; C=2.0 gives 71.43% accuracy ; C=5.0 gives 82.86% accuracy ; C=8.0 gives 80.00% accuracy'''
+    #C=1.0 gives 65.71% accuracy ; C=2.0 gives 71.43% accuracy ; C=5.0 gives 82.86% accuracy ; C=8.0 gives 80.00% accuracy
+    '''
+    model = RandomForestClassifier(n_estimators=100, max_depth=None, class_weight="balanced", random_state=42)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
