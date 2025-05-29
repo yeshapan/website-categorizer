72a317ff8d3f853f8aa85a776427f3c450865bd4
Yesha Pandya <yesha.pandya@outlook.com>
2025-05-29 16:08:26 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,5 @@
 #streamlit UI
 
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
 from categorizer.model import predict_category
